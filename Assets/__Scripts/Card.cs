6f04dce23ca1d6c81c93a3de5d7b383d6d89ba35
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-11-10 20:03:58 -0500

    P.555
    About to start making the score work

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -15,6 +15,12 @@ public class Card : MonoBehaviour {
 	public GameObject back;     // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+	// List of the SpriteRenderer Components of this GameObject and its children 
+	public SpriteRenderer[] spriteRenderers;
+
+	void Start() {
+		SetSortOrder(0); // Ensures that the card starts properly depth sorted
+	}
 	
 	// property
 	public bool faceUP {
@@ -25,6 +31,55 @@ public class Card : MonoBehaviour {
 			back.SetActive(!value);
 		}
 	}	
+	// If spriteRenderers is not yet defined, this function defines it 
+	public void PopulateSpriteRenderers() {
+	// If spriteRenderers is null or empty
+	if (spriteRenderers == null || spriteRenderers.Length == 0) {
+		// Get SpriteRenderer Components of this GameObject and its children
+		spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	// Sets the sortingLayerName on all SpriteRenderer Components 
+	public void SetSortingLayerName(string tSLN) {
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	// Sets the sortingOrder of all SpriteRenderer Components
+	public void SetSortOrder(int sOrd) {
+		PopulateSpriteRenderers ();
+
+		// The white background of the card is on bottom (sOrd)
+		// On top of that are all the pips, decorators, face, etc. (sOrd+1)
+		// The back is on top so that when visisble, it covers the rest (sOrd+2)
+
+		// Iterate through all the spriteRenderers as tSR 
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			if (tSR.gameObject == this.gameObject) {
+				// If the gameObject is this.gameObject, it's the background tSR.sortingOrder = sOrd; // Set its order to sOrd
+				continue; // And continue to the next iteration of the loop
+			
+			}
+			// Each of the children of this GameObject are named 
+			// switch based on the names
+			switch (tSR.gameObject.name) {
+			case "back": // if the name is "back"
+				tSR.sortingOrder = sOrd + 2;
+				// ^ Set it to the highest layer to cover everything else 
+				break;
+			case "face": // if the name is "face" 
+			default: // or if it's anything else
+				tSR.sortingOrder = sOrd + 1;
+		// ^ Set it to the middle layer to be above the background 
+				break;
+			}
+		}
+	}
+
 } // class Card
 
 [System.Serializable]
@@ -40,5 +95,11 @@ public class CardDefinition{
 	public string	face;	//sprite to use for face cart
 	public int		rank;	// value from 1-13 (Ace-King)
 	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used					
+					pips = new List<Decorator>();  // Pips Used
+
+	// Virtual methods can be overridden by subclass methods with the same name
+	virtual public void OnMouseUpAsButton() {
+		print (name); // When clicked, this outputs the card name
+	}
+
 } // Class CardDefinition
---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum CardState {
+	drawpile,
+	tableau,
+	target,
+	discard
+}
+
+public class CardProspector : Card { // Make sure CardProspector extends Card 
+	// This is how you use the enum CardState
+	public CardState state = CardState.drawpile;
+	// The hiddenBy list stores which other cards will keep this one face down
+	public List<CardProspector> hiddenBy = new List<CardProspector>();
+	// LayoutID matches this card to a Layout XML id if it's a tableau card
+	public int layoutID;
+	// The SlotDef class stores information pulled in from the LayoutXML <slot>
+	public SlotDef slotDef;
+}
+
